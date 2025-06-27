28bf30792efbabd86aebd727a732a636d3f61a2f
angelo <angelolorensi@gmail.com>
2025-06-26 22:10:13 -0300

    feat: minor adjustments to the models

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -12,17 +14,17 @@ class Product extends Model
     protected $primaryKey = 'product_id';
     protected $table = 'products';
 
-    public function productCategory()
+    public function productCategory(): BelongsTo
     {
         return $this->belongsTo(ProductCategory::class, 'product_category_id', 'product_category_id');
     }
 
-    public function slots()
+    public function slots(): HasMany
     {
         return $this->hasMany(Slot::class, 'product_id', 'product_id');
     }
 
-    public function transactions()
+    public function transactions(): HasMany
     {
         return $this->hasMany(Transaction::class, 'product_id', 'product_id');
     }
