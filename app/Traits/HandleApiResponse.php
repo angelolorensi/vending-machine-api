6b15491283c7e9aaadcb5441fbe6c1dddfb44848
Angelo Lorensi <angelolorensi@gmail.com>
2025-06-30 20:24:25 -0300

    feat: creates api response handler

---
+++ app/Traits/HandleApiResponse.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\JsonResponse;
+use Throwable;
+
+trait HandleApiResponse
+{
+    protected function handleResponse(callable $callback): JsonResponse
+    {
+        try {
+            $result = $callback();
+
+            return response()->json([
+                'success' => true,
+                'data' => $result
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'success' => false,
+                'message' => $e->getMessage()
+            ], $e->getCode() ?: 500);
+        }
+    }
+}
